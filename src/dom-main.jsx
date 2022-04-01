235928e92863f2c9c6ae733a947ec3bc5491087c
Dilip Kr. Shukla <dilip.shukla@tre.se>
2022-04-01 20:21:44 +0200

    add default export (#37)

--- src/dom-main.jsx
+++ src/dom-main.jsx
@@ -1,44 +1,7 @@
 /** @jsx h */
 import { h, hydrate } from "nano-jsx";
 
-async function hydrateHero() {
-  const mountElement = document.querySelector("jspm-package-hero");
 
-  if (mountElement) {
-    const { exports, name, version, description, updated, types } = mountElement.dataset;
-    const parsedExports = exports ? JSON.parse(exports) : [];
-    const { Hero } = await import("./hero.js");
-    hydrate(
-      <Hero
-        exports={parsedExports}
-        name={name}
-        version={version}
-        description={description}
-        updated={updated}
-        types={types}
-      />,
-      mountElement,
-    );
-  }
-}
-
-async function hydrateExports() {
-  const mountElement = document.querySelector("jspm-aside-exports");
-
-  if (mountElement) {
-    const { exports, name, version } = mountElement.dataset;
-    const parsedExports = exports ? JSON.parse(exports) : [];
-    const { Exports } = await import("./exports.js");
-    hydrate(
-      <Exports
-        exports={parsedExports}
-        name={name}
-        version={version}
-      />,
-      mountElement,
-    );
-  }
-}
 
 async function hydrateRoot() {
   const mountElement = document.querySelector("jspm-package-root");
@@ -46,7 +9,7 @@ async function hydrateRoot() {
   if (mountElement) {
     const { exports, name, version, features, links, maintainers, readme } = mountElement.dataset;
     const parsedExports = exports ? JSON.parse(exports) : [];
-    const { DomRoot } = await import("./dom-root.js");
+    const { DomRoot } = await import("@jspm/packages/dom-root");
     
     hydrate(
       <DomRoot name={name} version={version} exports={parsedExports} features={JSON.parse(features)} links={JSON.parse(links)} maintainers={JSON.parse(maintainers)} readme={readme} />,
