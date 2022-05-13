ba7eebfe2786bf1c4fdb61d4e8a303023e94f76e
Dilip Kr. Shukla <dilip.shukla@tre.se>
2022-05-13 09:18:57 +0200

    auto generate static resources list

---
+++ generate-static-resources-map.js
@@ -0,0 +1,50 @@
+import { Generator } from "@jspm/generator";
+import { readdir, writeFile } from "node:fs/promises";
+import mkdirp from "mkdirp";
+import { dirname, parse } from "path";
+
+const generator = new Generator({
+  env: ["browser", "production"],
+});
+
+const dependencyMap = await generator.traceInstall("@jspm/packages");
+
+mkdirp.sync(dirname(process.env.TARGET));
+
+const { dir: CURRENT_DIRECTORY } = parse(import.meta.url);
+
+const { staticDeps, dynamicDeps } = dependencyMap;
+
+const OUTPUT_DIR = "lib";
+const DEST_PREFIX = `${CURRENT_DIRECTORY}/${OUTPUT_DIR}`;
+const staticResources = {};
+
+[...staticDeps, ...dynamicDeps].filter((dependency) =>
+  dependency.startsWith(CURRENT_DIRECTORY)
+).sort().forEach((dependency) => {
+  const modulePath = dependency.replace(CURRENT_DIRECTORY, ".");
+  const subpath = dependency.replace(DEST_PREFIX, "");
+
+  staticResources[subpath] = modulePath;
+
+  /**
+   * @todo validate if sourcemap exists
+   */
+  staticResources[`${subpath}.map`] = `${modulePath}.map`;
+});
+
+const files = await readdir("images");
+for (const file of files) {
+  staticResources[`/${file}`] = `./images/${file}`;
+}
+
+const sortedStaticResources = {};
+
+Object.keys(staticResources).sort().forEach((key) => {
+  sortedStaticResources[key] = staticResources[key];
+});
+
+await writeFile(
+  process.env.TARGET,
+  JSON.stringify(sortedStaticResources, null, 2),
+);
--- src/server-main.jsx
+++ src/server-main.jsx
@@ -1,222 +1,28 @@
 /** @jsx h */
-
+import {
+  contentType,
+  lookup,
+} from "https://deno.land/x/media_types@v2.12.3/mod.ts";
 import nano, { h, renderSSR } from "nano-jsx";
 import dayjs from "dayjs";
 import dayjsPluginRelativeTime from "dayjs/plugin/relativeTime";
 import { Semver } from "sver";
 import { SsrRoot } from "@jspm/packages/ssr-root";
 import { Home } from "@jspm/packages/home";
-import {
-  pageServingHeaders,
-} from "@jspm/packages/utils";
+import { pageServingHeaders } from "@jspm/packages/utils";
 import { render } from "@jspm/packages/renderer";
 import { FEATURED_PACKAGES } from "@jspm/packages/featured-packages-list";
 import { features, parseURL } from "@jspm/packages/package-quality-check";
 
 const { Helmet } = nano;
 
+const staticResourcesFile = await Deno.readTextFile(
+  "./lib/static-resources.json",
+);
 const staticResources = {
-  "/style.css": { path: "./style.css", contentType: "text/css; charset=utf-8" },
-  "/dom-main.js": {
-    path: "./lib/dom-main.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/dom-main.js.map": {
-    path: "./lib/dom-main.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/main.js": {
-    path: "./lib/main.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/main.js.map": {
-    path: "./lib/main.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/dom-root.js": {
-    path: "./lib/dom-root.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/dom-root.js.map": {
-    path: "./lib/dom-root.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/importmap-dialog.js": {
-    path: "./lib/importmap-dialog.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/importmap-dialog.js.map": {
-    path: "./lib/importmap-dialog.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/statehash.js": {
-    path: "./lib/statehash.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/statehash.js.map": {
-    path: "./lib/statehash.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/header.js": {
-    path: "./lib/header.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/header.js.map": {
-    path: "./lib/header.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/package.js": {
-    path: "./lib/package.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/package.js.map": {
-    path: "./lib/package.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/readme.js": {
-    path: "./lib/readme.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/readme.js.map": {
-    path: "./lib/readme.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/aside.js": {
-    path: "./lib/aside.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/aside.js.map": {
-    path: "./lib/aside.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/footer.js": {
-    path: "./lib/footer.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/footer.js.map": {
-    path: "./lib/footer.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/separator.js": {
-    path: "./lib/separator.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/separator.js.map": {
-    path: "./lib/separator.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/exports.js": {
-    path: "./lib/exports.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/exports.js.map": {
-    path: "./lib/exports.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/hero.js": {
-    path: "./lib/hero.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/hero.js.map": {
-    path: "./lib/hero.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/generate-statehash.js": {
-    path: "./lib/generate-statehash.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/generate-statehash.js.map": {
-    path: "./lib/generate-statehash.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/logo.js": {
-    path: "./lib/logo.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/logo.js.map": {
-    path: "./lib/logo.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/search.js": {
-    path: "./lib/search.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/search.js.map": {
-    path: "./lib/search.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/nav.js": {
-    path: "./lib/nav.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/nav.js.map": {
-    path: "./lib/nav.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/importmap-generator.js": {
-    path: "./lib/importmap-generator.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/importmap-generator.js.map": {
-    path: "./lib/importmap-generator.js.map",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/imports-hash-store.js": {
-    path: "./lib/imports-hash-store.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/featured-packages-list.js": {
-    path: "./lib/featured-packages-list.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/renderer.js": {
-    path: "./lib/renderer.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/package-quality-check.js": {
-    path: "./lib/package-quality-check.js",
-    contentType: "application/javascript; charset=utf-8",
-  },
-  "/icon-add.svg": {
-    path: "./images/icon-add.svg",
-    contentType: "image/svg+xml; charset=utf-8",
-  },
-  "/icon-check.svg": {
-    path: "./images/icon-check.svg",
-    contentType: "image/svg+xml; charset=utf-8",
-  },
-  "/icon-typescript-logo.svg": {
-    path: "./images/icon-typescript-logo.svg",
-    contentType: "image/svg+xml; charset=utf-8",
-  },
-  "/icon-distributed.png": {
-    path: "./images/icon-distributed.png",
-    contentType: "image/png",
-  },
-  "/icon-external-link.png": {
-    path: "./images/icon-external-link.png",
-    contentType: "image/png",
-  },
-  "/icon-arrow-down.png": {
-    path: "./images/icon-arrow-down.png",
-    contentType: "image/png",
-  },
-  "/icon-importmap.png": {
-    path: "./images/icon-importmap.png",
-    contentType: "image/png",
-  },
-  "/npm-n-block-32.jpeg": {
-    path: "./images/npm-n-block-32.jpeg",
-    contentType: "image/png",
-  },
-  "/npm-n-block-16.png": {
-    path: "./images/npm-n-block-16.png",
-    contentType: "image/png",
-  },
-  "/favicon.ico": {
-    path: "./favicon.ico",
-    contentType: "image/vnd.microsoft.icon",
-  },
+  "/style.css": "./style.css",
+  "/favicon.ico": "./favicon.ico",
+  ...JSON.parse(staticResourcesFile),
 };
 
 async function generateHTML(
@@ -289,18 +95,19 @@ async function requestHandler(request) {
     const packageName = searchParams.get("q");
 
     if (packageName) {
-      return redirectToJSPMPackageVersion(packageName)
+      return redirectToJSPMPackageVersion(packageName);
     }
 
     const pathSegments = removeSlashes(pathname).split("/");
+    
     const staticResource =
       staticResources[`/${pathSegments[pathSegments.length - 1]}`];
 
     if (staticResource) {
-      const response = await Deno.readFile(staticResource.path);
+      const response = await Deno.readFile(staticResource);
 
       return new Response(response, {
-        headers: { "content-type": staticResource.contentType },
+        headers: { "content-type": contentType(lookup(staticResource)) },
       });
     }
 
